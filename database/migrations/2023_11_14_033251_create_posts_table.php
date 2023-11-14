a935043c58c300f2fcb4e09b22afb6727de73dbc
Diego Maldonado <dj_maldonado19@hotmail.es>
2023-11-13 22:44:05 -0600

    Adding connection with postgreslq , migrations usage, and web vew, with PostController

--- database/migrations/2023_11_14_033251_create_posts_table.php
+++ database/migrations/2023_11_14_033251_create_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
 
-            $table->usignedBigInteger('user_id');
+            $table->unsignedBigInteger('user_id');
             $table->string('title');
             $table->string('slug')->unique();
             $table->text('content');
