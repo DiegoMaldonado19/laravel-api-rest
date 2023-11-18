5a5c1e1241cbe3ec0dd8d549c3397de31cf6ad98
Diego Maldonado <dj_maldonado19@hotmail.es>
2023-11-18 11:01:36 -0600

    Adding planification of v2, adding controllers and resources

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use  App\Http\Controllers\Api\v1\PostController as PostV1;
+use  App\Http\Controllers\Api\v2\PostController as PostV2;
+
+/*
 Route::apiResource('v1/posts', App\Http\Controllers\Api\v1\PostController::class)
 ->only('index','show', 'destroy');
+*/
+
+//v1 routes
+Route::apiResource('v1/posts', PostV1::class)
+->only(['index','show', 'destroy']);
+
+//v2 routes
+Route::apiResource('v2/posts', PostV2::class)
+->only(['index','show']);
