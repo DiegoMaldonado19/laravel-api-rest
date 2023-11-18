d615593d7bb0087459ecbae47862728596498484
Diego Maldonado <dj_maldonado19@hotmail.es>
2023-11-18 13:37:13 -0600

    Adding first step to login authentication management

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request){
+        $this->validateLogin($request);
+
+        // login configuration
+
+        // login true
+
+        
+
+        // login false
+    }
+
+    public function validateLogin(Request $request){
+        return $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'name' => 'required' // to know where the user are connecting
+        ]);
+    }
+}
