7294f2a5ebfc7fc9c235c9dfdf6c22452a6a37d5
Diego Maldonado <dj_maldonado19@hotmail.es>
2023-11-18 14:33:23 -0600

    Finishing authentication in the laravel api

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -16,10 +16,18 @@ class LoginController extends Controller
         // login configuration
 
         // login true
-
-        
+        if( Auth::attempt($request->only('email', 'password')) ){
+            return response()->json([
+                'token' => $request->user()->createToken($request->name)->plainTextToken,
+                'message' => 'Success'
+            ], 200);
+        }
 
         // login false
+
+        return response()->json([
+            'message' => "Unauthorized"
+        ], 401);
     }
 
     public function validateLogin(Request $request){
